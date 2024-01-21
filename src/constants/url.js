f5002ea523dc3abf8b8aab8d3b524d169ebc9f0e
w3villa-vishal-maurya <vishalprakash.maurya@w3villa.com>
2024-01-21 22:32:13 +0530

    remove http

--- src/constants/url.js
+++ src/constants/url.js
@@ -1,4 +1,4 @@
 export const API_KEY= process.env.REACT_APP_TMDB_APIKEY;
-export const API_KEY_OMBD= process.env.API_KEY_OMBD;
+export const API_KEY_OMBD= process.env.REACT_APP_API_KEY_OMBD;
 export const BASE_IMAGE_URL = 'https://image.tmdb.org/t/p/original';
 export const YOUTUBE_URL = "https://www.youtube.com/watch?v=";
